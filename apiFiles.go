cfd178f9ffbf5b83dfb35f5b2f78a4c020bb09d8
George Antoniadis <george@noodles.gr>
2015-01-08 16:12:01 +0200

    Accept multiple files

--- apiFiles.go
+++ apiFiles.go
@@ -15,24 +15,26 @@ import (
 
 func ApiFilesPost(r *http.Request, enc encoder.Encoder, store Store, parms martini.Params) (int, []byte) {
 	db := GetDbSession()
-	var userFile UserFile
+	var userFiles []UserFile
 
 	body, _ := ioutil.ReadAll(r.Body)
 	r.Body.Close()
-	err := json.Unmarshal(body, &userFile)
+	err := json.Unmarshal(body, &userFiles)
 
 	if err != nil {
 		return http.StatusNotFound, encoder.Must(enc.Encode(
 			NewError(ErrCodeNotExist, "Could not decode body")))
 	}
 
-	err = db.SelectOne(&userFile, "select * from users_files where user_id=? and full_path_hash=?", userFile.UserID, userFile.FullPathHash)
-	if err == sql.ErrNoRows {
-		userFile.ID = uuid.Formatter(uuid.NewV4(), uuid.CleanHyphen)
-		db.Insert(&userFile)
-	} else if err != nil {
-		return http.StatusNotFound, encoder.Must(enc.Encode(
-			NewError(ErrCodeNotExist, "Could not insert file record")))
+	for userFile_i, userFile := range userFiles {
+		err = db.SelectOne(&userFiles[userFile_i], "select * from users_files where user_id=? and full_path_hash=?", userFile.UserID, userFile.FullPathHash)
+		if err == sql.ErrNoRows {
+			userFiles[userFile_i].ID = uuid.Formatter(uuid.NewV4(), uuid.CleanHyphen)
+			db.Insert(&userFile)
+		} else if err != nil {
+			return http.StatusNotFound, encoder.Must(enc.Encode(
+				NewError(ErrCodeNotExist, "Could not insert file record")))
+		}
 	}
-	return http.StatusOK, encoder.Must(enc.Encode(userFile))
+	return http.StatusOK, encoder.Must(enc.Encode(userFiles))
 }

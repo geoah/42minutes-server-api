7c27963267eab0b9f096f14b762f24ab336116ab
George Antoniadis <george@noodles.gr>
2015-01-14 02:18:56 +0200

    Fix UserEpisode structure

--- models/user_episode.go
+++ models/user_episode.go
@@ -1,9 +1,10 @@
 package models
 
 type UserEpisode struct {
-	UserID    int    `json:"user_id" db:"user_id"`
+	UserID    string `json:"user_id" db:"user_id"`
 	ShowID    int    `json:"show_id" db:"show_id"`
 	SeasonID  int    `json:"season" db:"season"`
 	EpisodeID int    `json:"episode" db:"episode"`
+	Watched   bool   `json:"watched" db:"watched"`
 	Path      string `json:"path" db:"path"`
 }

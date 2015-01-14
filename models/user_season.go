377070673f47212338c00b0eae0b2c88caa16109
George Antoniadis <george@noodles.gr>
2015-01-14 02:18:46 +0200

    Fix UserSeason structure

--- models/user_season.go
+++ models/user_season.go
@@ -1,7 +1,7 @@
 package models
 
 type UserSeason struct {
-	UserID   int    `json:"user_id" db:"user_id"`
+	UserID   string `json:"user_id" db:"user_id"`
 	ShowID   int    `json:"show_id" db:"show_id"`
 	SeasonID int    `json:"season" db:"season"`
 	Path     string `json:"path" db:"path"`
